574c7f2ccf99c01665bfb468910e569d7d895dbe
Petr Girich <petr.girich@gmail.com>
2024-02-22 17:32:18 +0300

    try iterate VObj

--- src/view/v_inventory.tsx
+++ src/view/v_inventory.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { iList } from '../model/m_data'
+import VObj from './v_obj'
+import CObj from '../model/m_obj'
 
+interface propsVInventory {
+  children: React.ReactNode
+}
 export default function VInventory() {
   return (
     <div className="VInventory">
       <div>On Sale....</div>
+      { Array.from(iList.values()).map(key => {
+              <VObj obj={key} />
+      } ) }
     </div>
   )
 }
--- src/view/v_obj.tsx
+++ src/view/v_obj.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CObj from '../model/m_obj'
 
-export default function VObj() {
+interface propsVObj {
+  obj : CObj
+}
+export default function VObj(props:propsVObj) {
   return (
     <div className="VObj">
-      <div>Object</div>
+      <div>Object {props.obj.caption} </div>
     </div>
   )
 }
